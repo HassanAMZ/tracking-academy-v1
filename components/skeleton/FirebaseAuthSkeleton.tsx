58755c805a0728e0113f336cc131c054918c608a
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2023-10-24 16:33:18 +0800

    Updated Blog MDX for 004, 005

--- components/skeleton/FirebaseAuthSkeleton.tsx
+++ components/skeleton/FirebaseAuthSkeleton.tsx
@@ -5,10 +5,9 @@ export const FirebaseAuthSkeleton = () => {
  return (
   <ContainerLayout className='backgroundOverlay p-2'>
    <div className='flex flex-col items-center gap-2 h-[50vh] justify-center animate-pulse'>
-    <div className='bg-gray-300 rounded w-1/2 h-6 mb-4'></div>
-    <div className='flex gap-4 mt-4'>
-     <div className='bg-gray-300 rounded w-48 h-10'></div>
-     <div className='bg-gray-300 rounded w-48 h-10'></div>
+    <div className='bg-gray-300 rounded w-1/3 h-4'></div>
+    <div className='flex gap-4 mt-2'>
+     <div className='bg-gray-300 rounded w-[50vw] h-10'></div>
     </div>
    </div>
   </ContainerLayout>
