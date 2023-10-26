3832648465b8409490a4ef905c6760a068f751d8
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2023-10-26 23:22:27 +0800

    Updated BlogSeach Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 
 const BgPolygon1 = (
  <div
-  className='fixed inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 animate-moving1'
+  className='animate-pulse fixed inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 '
   aria-hidden='true'>
   <div
    className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
@@ -68,7 +68,7 @@ const BgPolygon1 = (
 
 const BgPolygon2 = (
  <div
-  className='fixed inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)] animate-moving2'
+  className='fixed inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)] animate-pulse'
   aria-hidden='true'>
   <div
    className='relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]'
--- components/blog/SingleBlogCard.tsx
+++ components/blog/SingleBlogCard.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 import generateBrightColor from "@/components/utils/generateBrightColor";
 import formatDate from "@/components/seo/formatDate";
 import { SingleBlogCardProps } from "@/types/index";
+
 const BlogCard: React.FC<SingleBlogCardProps> = ({
  post,
  type,
@@ -18,18 +19,19 @@ const BlogCard: React.FC<SingleBlogCardProps> = ({
 }) => {
  const renderContent = () => (
   <>
-   {isMain ? (
-    <div className={`backgroundOverlay !bg-purple-600  text-white p-6`}>
-     <Heading2xl className='line-clamp-2 py-2 '>{post.title}</Heading2xl>
-    </div>
-   ) : (
-    <div
-     className={`backgroundOverlay p-6 `}
-     style={{ backgroundColor: generateBrightColor(0.05) }}>
-     <Headingxl className='line-clamp-2 '>{post.title}</Headingxl>
-    </div>
-   )}
-
+   <div
+    className={`backgroundOverlay p-6 !shadow-none`}
+    style={{
+     backgroundColor: generateBrightColor(0.08),
+     position: "relative",
+    }}>
+    {isMain && (
+     <div className='absolute top-0 right-0 bg-green-500 text-white px-2 py-1 rounded-md'>
+      New
+     </div>
+    )}
+    <Headingxl className='line-clamp-2'>{post.title}</Headingxl>
+   </div>
    <div className='textOpacity80 py-6 px-6 border-md'>
     <Paragraphsm className='line-clamp-3'>{post.description}</Paragraphsm>
     <Paragraphxs className='md:pt-6 pt-4 pb-2 flex justify-between'>
@@ -38,18 +40,18 @@ const BlogCard: React.FC<SingleBlogCardProps> = ({
    </div>
   </>
  );
+
  return (
   <React.Fragment>
-   <div
-    className={`shadow-md hover:animate-pulse ${
-     isMain ? "hidden md:flex" : ""
-    } ${className}`}>
-    <Link href={`/${type}/${post.slug}`} className=''>
-     <section className='grid backgroundOverlay h-full w-full'>
-      {renderContent()}
-     </section>
-    </Link>
-   </div>
+   {post && ( // Check if post is defined
+    <div className={`shadow-md hover:animate-pulse  ${className}`}>
+     <Link href={`/${type}/${post.slug}`} className=''>
+      <section className='grid backgroundOverlay h-full w-full'>
+       {renderContent()}
+      </section>
+     </Link>
+    </div>
+   )}
   </React.Fragment>
  );
 };
